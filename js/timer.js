9fca085e862a1854caae1468c85dbb191c51d92b
nightingale16066 <nightingale16066@gmail.com>
2022-09-17 16:26:40 +0300

    bonus2

--- js/timer.js
+++ js/timer.js
@@ -1,12 +1,34 @@
 const timerField = document.querySelector('.timer');
-const heroText = document.querySelector('.hero__text');
 const heroTimer = document.querySelector('.hero__timer');
-const timerItemDays = document.querySelector('.timer__item_days');
-const timerItemSeconds = document.querySelector('.timer__item_seconds');
 const deadline = timerField.dataset.timerDeadline;
+const dataTimer = document.querySelector('[data-timer-deadline]');
+
+
+const renderPage = () => {
+  heroTimer.insertAdjacentHTML('beforebegin', `<p class="hero__text">Действует скидка 20% на туры от 6&nbsp;человек</p>`);
+
+  heroTimer.insertAdjacentHTML('beforeend', `
+    <p class="timer__title">До конца акции осталось:</p>
+    <p class="timer__item timer__item_days">
+      <span class="timer__count timer__count_days">2</span>
+      <span class="timer__units timer__units_days">дня</span></p>
+    <p class="timer__item timer__item_hours">
+      <span class="timer__count timer__count_hours">05</span>
+      <span class="timer__units timer__units_hours">часов</span></p>
+    <p class="timer__item timer__item_minutes">
+      <span class="timer__count timer__count_minutes">12</span>
+      <span class="timer__units timer__units_minutes">минут</span></p>
+    <p class="timer__item timer__item_seconds" style="display: none;">
+      <span class="timer__count timer__count_seconds">10</span>
+      <span class="timer__units timer__units_seconds">секунд</span></p> 
+  `);
+}
 
 
 const timer = deadline => {
+  const heroText = document.querySelector('.hero__text');
+  const timerItemDays = document.querySelector('.timer__item_days');
+  const timerItemSeconds = document.querySelector('.timer__item_seconds');
   const timerCountDays = document.querySelector('.timer__count_days');
   const timerUnitsDays = document.querySelector('.timer__units_days');
   const timerCountHours = document.querySelector('.timer__count_hours');
@@ -15,6 +37,7 @@ const timer = deadline => {
   const timerUnitsMinutes = document.querySelector('.timer__units_minutes');
   const timerCountSeconds = document.querySelector('.timer__count_seconds');
   const timerUnitsSeconds = document.querySelector('.timer__units_seconds');
+  
 
   const setRightWord = (time, timeUnit) => {
     const words = {
@@ -85,4 +108,10 @@ const timer = deadline => {
   update();
 }
 
-timer(deadline);
+
+if (dataTimer) {
+  renderPage();
+  timer(deadline);
+}
+
+
